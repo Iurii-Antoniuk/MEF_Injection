4e794e65f2ce6082b7b89022b2a145e7eff87487
Iurii-Antoniuk <iurii.antoniuk@gmail.com>
2020-04-06 16:23:00 +0200

    initial commit

---
+++ MEF_Injection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEF_Injection
+{
+    class Program
+    {
+        private MefHost _host;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+
+            Console.ReadKey();
+        }
+
+        private void Run()
+        {
+            _host = new MefHost();
+            HelloSayer service = _host.Container.GetExportedValue<HelloSayer>();
+            service.SayHello();
+
+        }
+    }
+
+    internal class MefHost
+    {
+        private CompositionContainer _container = null;
+        public CompositionContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    var catalog = new DirectoryCatalog(".", "MEF_Injection.*");
+                    _container = new CompositionContainer(catalog);
+                }
+                return _container;
+            }
+        }
+
+    }
+
+    [Export]
+    internal class HelloSayer
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Bonjour ! Yob ty!");
+        }
+    }
+}
